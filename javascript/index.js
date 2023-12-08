25d62f0fb683e2920fff525a4e48a516a83457b7
Rubens Kuhn <152165400+rubenskuhn@users.noreply.github.com>
2023-12-08 09:29:34 +0100

    js-challange

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+//
+//Ways to run JS files:
+//1. In the terminal: either OS or VS
+//2. In VS code, with Run without Debugging (select node)
+
+console.log("Do you see me");
+
+const isANumber = 1;
+let amIaNumber = 2;
+let students = 16;
+
+// Sum
+let sum = amIaNumber + isANumber + students;
+console.log("Sum", sum);
+console.log("Type of sum", typeof sum);
+
+//12, undefine
+
+// 2 Strings
+const myString = "Hi class!";
+const myString2 = "Really match :S";
+console.log("mySring + myString2: ", myString + myString2);
+
+// Is one variable is
+
+console.log("hola");
+
+console.log("ADDing 1: ");
+console.log("1. Initial amIaNumber ");
+
+console.log("---------- Subtract ----------");
+let sub = amIaNumber - isANumber - students;
+console.log("sub", sub);
